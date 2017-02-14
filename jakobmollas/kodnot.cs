e44e51bde767b26d318648248efb220e4d5fb43c
Möllås Jakob (Consultant) <u032060@corp.saab.se>
2017-02-14 09:49:50 +0100

    Added files

---
+++ jakobmollas/kodnot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Challenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            new[] {10, 50, 1000}.Select(antal =>
+				(from fib in Enumerable.Range(1, antal)
+						let fibonacciStörreÄnEtt = Enumerable.Range(1, fib)
+							.Aggregate(new {Denna = BigInteger.One, Förra = BigInteger.One},
+								(n, i) => new {Denna = n.Denna + n.Förra, Förra = n.Denna}).Denna
+						let delmål = fibonacciStörreÄnEtt.ToString().Sum(c => c - 48) % 10
+						select delmål)
+					.Sum() % 10)
+                .ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
